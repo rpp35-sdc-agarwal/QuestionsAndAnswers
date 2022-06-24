3579f6b78fe05e8f0e04313afe75ff381e2c3e2c
Eli Veilleux <elibchv@gmail.com>
2022-06-23 20:14:11 -0700

    Schema update to include required fields

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -1,5 +1,10 @@
-CREATE TABLE "Questions" (
+DROP TABLE IF EXISTS "questions";
+DROP TABLE IF EXISTS "answers";
+DROP TABLE IF EXISTS "photos";
+
+CREATE TABLE "questions" (
   "question_id" SERIAL UNIQUE PRIMARY KEY,
+  "product_id" int NOT NULL,
   "question_body" varchar(1000) NOT NULL,
   "question_date" varchar NOT NULL,
   "username" varchar(60) NOT NULL,
@@ -8,20 +13,23 @@ CREATE TABLE "Questions" (
   "reported" boolean
 );
 
-CREATE TABLE "Answers" (
+CREATE TABLE "answers" (
   "answer_id" SERIAL PRIMARY KEY,
   "answer_body" varchar(1000) NOT NULL,
+  "answer_date" varchar NOT NULL,
   "username" varchar(60) NOT NULL,
   "email" varchar(60) NOT NULL,
-  "question_id" int
+  "question_id" int,
+  "reported" boolean,
+  "helpful" int
 );
 
-CREATE TABLE "Photos" (
+CREATE TABLE "photos" (
   "photo_id" SERIAL PRIMARY KEY,
   "photo_url" varchar NOT NULL,
   "answer_id" int
 );
 
-ALTER TABLE "Answers" ADD FOREIGN KEY ("question_id") REFERENCES "Questions" ("question_id");
+ALTER TABLE "answers" ADD FOREIGN KEY ("question_id") REFERENCES "questions" ("question_id");
 
-ALTER TABLE "Photos" ADD FOREIGN KEY ("answer_id") REFERENCES "Answers" ("answer_id");
+ALTER TABLE "photos" ADD FOREIGN KEY ("answer_id") REFERENCES "answers" ("answer_id");
