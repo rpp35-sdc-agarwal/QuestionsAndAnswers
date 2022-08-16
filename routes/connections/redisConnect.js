d4b2229f4b91a74cafd52bf9a9c6bcc0d4d763b0
Eli Veilleux <elibchv@gmail.com>
2022-08-15 21:13:06 -0700

    Redis tests ioredis

--- routes/connections/redisConnect.js
+++ routes/connections/redisConnect.js
@@ -1,4 +1,4 @@
-const redis = require('redis');
-const client = redis.createClient({url:'redis://ubuntu@52.52.238.201:6739'});
+const Redis = require('ioredis');
+const client = new Redis({6739, 'ubuntu@52.52.238.201'});
 
 module.exports = client;
