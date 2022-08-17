97f1152b9152577dc2aafb92778063f61f4f1a2b
Eli Veilleux <elibchv@gmail.com>
2022-08-17 12:10:07 -0700

    redis connect obj updated

--- routes/connections/redisConnect.js
+++ routes/connections/redisConnect.js
@@ -1,4 +1,8 @@
 const Redis = require('ioredis');
-const client = new Redis(6739, 'ubuntu@52.52.238.201');
+const client = new Redis({
+  port: 6739,
+  host: 'ubuntu@52.52.238.201',
+  connectTimeout: 10000
+});
 
 module.exports = client;
